784da7ea38e0958d0b8aa15409ef2b4529e8ee4c
Dimitrios - Georgios Kontopoulos <dgkontopoulos@gmail.com>
2012-05-26 21:44:02 +0300

    Reorganized the database updating script. In case of a database being updated at the time of downloading, the script will automatically re-download it.

--- db_update.sh
+++ db_update.sh
@@ -8,7 +8,7 @@
 #Author: Dimitrios - Georgios Kontopoulos#
 ##########################################
 
-###################################################################### 
+######################################################################
 #This program is free software: you can redistribute it and/or modify#
 #it under the terms of the GNU General Public License as             #
 #published by the Free Software Foundation, either version 3 of the  #
@@ -17,95 +17,85 @@
 #For more information, see http://www.gnu.org/licenses/.             #
 ######################################################################
 
-set -e
 
 ##########################################################################
 #Get the Swiss-Prot db, format it and also copy it to the UniProt folder.#
 ##########################################################################
 mkdir /usr/local/databases/Swissprot.1/
 cd /usr/local/databases/Swissprot.1/
-nice -n +20 wget ftp://ftp.ebi.ac.uk/pub/databases/uniprot/knowledgebase/uniprot_sprot.fasta.gz
 
-################################################################
-#If wget does not finish properly, mail someone to take a look.#
-################################################################
-if [ "$?" -ne "0" ]; then
-   echo 'The Swiss-prot database failed to download properly.' > /tmp/tmp
-   mail dimikont2@mbg.duth.gr -s 'DB fetching error' < /tmp/tmp
-   rm /tmp/tmp
-else
-   nice -n +20 gunzip uniprot_sprot.fasta.gz
-   mkdir /usr/local/databases/UniProt.1/
-   cp uniprot_sprot.fasta ../UniProt.1/
-   nice -n +20 makeblastdb -in uniprot_sprot.fasta -dbtype prot -parse_seqids
-   rm uniprot_sprot.fasta
-   
-   ##########################################################################
-   #Get the TrEMBL db, concatenate it with the Swiss-Prot one and format it.#
-   ##########################################################################
-   cd ../UniProt.1/
-   nice -n +20 wget ftp://ftp.ebi.ac.uk/pub/databases/uniprot/knowledgebase/uniprot_trembl.fasta.gz
-   
-   ################################################################
-   #If wget does not finish properly, mail someone to take a look.#
-   ################################################################
-   if [ "$?" -ne "0" ]; then
-      echo 'The TrEMBL database failed to download properly.' > /tmp/tmp
-      mail dimikont2@mbg.duth.gr -s 'DB fetching error' < /tmp/tmp
-      rm /tmp/tmp
+Swiss_status="999"
+while [ "$Swiss_status" -ne "0" ]; do
+   rm *.gz
+   nice -n +19 wget ftp://ftp.ebi.ac.uk/pub/databases/uniprot/knowledgebase/uniprot_sprot.fasta.gz
+   nice -n +19 gunzip uniprot_sprot.fasta.gz
+   Swiss_status="$?"
+done
+
+mkdir /usr/local/databases/UniProt.1/
+cp uniprot_sprot.fasta ../UniProt.1/
+nice -n +19 makeblastdb -in uniprot_sprot.fasta -dbtype prot -parse_seqids
+rm uniprot_sprot.fasta
+
+##########################################################################
+#Get the TrEMBL db, concatenate it with the Swiss-Prot one and format it.#
+##########################################################################
+cd ../UniProt.1/
+
+Uni_status="999"
+while [ "$Uni_status" -ne "0" ]; do
+   rm *.gz
+   nice -n +19 wget ftp://ftp.ebi.ac.uk/pub/databases/uniprot/knowledgebase/uniprot_trembl.fasta.gz
+   nice -n +19 gunzip uniprot_trembl.fasta.gz
+   Uni_status="$?"
+done
+
+cat uniprot_sprot.fasta uniprot_trembl.fasta > UniProt.fasta
+rm -rf uniprot_sprot.fasta uniprot_trembl.fasta
+nice -n +19 makeblastdb -in UniProt.fasta -dbtype prot -parse_seqids
+rm UniProt.fasta
+
+################################################
+#Check for running processes (blastp/psiblast).#
+################################################
+while true
+do
+   if (ps ax | grep -v grep | grep blastp) > /dev/null && ( ps ax | grep -v grep | grep psiblast) > /dev/null;
+   then
+      sleep 60
    else
-      nice -n +20 gunzip uniprot_trembl.fasta.gz
-      cat uniprot_sprot.fasta uniprot_trembl.fasta > UniProt.fasta
-      rm -rf uniprot_sprot.fasta uniprot_trembl.fasta
-      nice -n +20 makeblastdb -in UniProt.fasta -dbtype prot -parse_seqids
-      rm UniProt.fasta
-      
-      ################################################
-      #Check for running processes (blastp/psiblast).#
-      ################################################
-      while true
-      do
-         if (ps ax | grep -v grep | grep blastp) > /dev/null && ( ps ax | grep -v grep | grep psiblast) > /dev/null;
-         then
-            sleep 60
-         else
-            rm -rf /usr/local/databases/Swissprot/ && mv /usr/local/databases/Swissprot.1/ /usr/local/databases/Swissprot/
-            rm -rf /usr/local/databases/UniProt/ && mv /usr/local/databases/UniProt.1/ /usr/local/databases/UniProt/
-            break
-         fi
-      done
+      rm -rf /usr/local/databases/Swissprot/ && mv /usr/local/databases/Swissprot.1/ /usr/local/databases/Swissprot/
+      rm -rf /usr/local/databases/UniProt/ && mv /usr/local/databases/UniProt.1/ /usr/local/databases/UniProt/
+      break
    fi
-fi
+done
 
 ####################################
 #Get the nt database and format it.#
 ####################################
 mkdir /usr/local/databases/nt.1/
 cd /usr/local/databases/nt.1/
-nice -n +20 wget ftp://ftp.ncbi.nih.gov/blast/db/FASTA/nt.gz
 
-################################################################
-#If wget does not finish properly, mail someone to take a look.#
-################################################################
-if [ "$?" -ne "0" ]; then
-   echo 'The nt database failed to download properly.' > /tmp/tmp
-   mail dimikont2@mbg.duth.gr -s 'DB fetching error' < /tmp/tmp
-   rm /tmp/tmp
-else
-   nice -n +20 gunzip nt.gz
-   mv nt nt.fasta
-   nice -n +20 makeblastdb -in nt.fasta -dbtype nucl -parse_seqids
-   rm nt.fasta
-   
-   while true
-   do
-      if (ps ax | grep -v grep | grep blastn) > /dev/null;
-      then
-         sleep 60
-      else
-         rm -rf /usr/local/databases/nt/ && mv /usr/local/databases/nt.1/ /usr/local/databases/nt/
-         break
-      fi
-   done
-fi
+nt_status="999"
+while [ "$nt_status" -ne "0" ]; do
+   rm *.gz
+   nice -n +19 wget ftp://ftp.ncbi.nih.gov/blast/db/FASTA/nt.gz
+   nice -n +19 gunzip nt.gz
+   nt_status="$?"
+done
+
+mv nt nt.fasta
+nice -n +19 makeblastdb -in nt.fasta -dbtype nucl -parse_seqids
+rm nt.fasta
+
+while true
+do
+   if (ps ax | grep -v grep | grep blastn) > /dev/null;
+   then
+      sleep 60
+   else
+      rm -rf /usr/local/databases/nt/ && mv /usr/local/databases/nt.1/ /usr/local/databases/nt/
+      break
+   fi
+done
 exit 0
